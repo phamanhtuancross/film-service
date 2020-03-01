3261222ba753129456673856ef89ffaf36a4a092
Phạm Anh Tuấn <v.tuanpa28@vinid.net>
2020-03-02 01:16:43 +0700

    add chat with server api

--- src/pkg/api/v1/film-service.pb.go
+++ src/pkg/api/v1/film-service.pb.go
@@ -118,28 +118,121 @@ func (m *FilmCategory) GetImageUrl() string {
 	return ""
 }
 
+type ChatStreamMessage struct {
+	Name                 string   `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	Message              string   `protobuf:"bytes,2,opt,name=message,proto3" json:"message,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *ChatStreamMessage) Reset()         { *m = ChatStreamMessage{} }
+func (m *ChatStreamMessage) String() string { return proto.CompactTextString(m) }
+func (*ChatStreamMessage) ProtoMessage()    {}
+func (*ChatStreamMessage) Descriptor() ([]byte, []int) {
+	return fileDescriptor_34fd3b300f068c9c, []int{2}
+}
+
+func (m *ChatStreamMessage) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_ChatStreamMessage.Unmarshal(m, b)
+}
+func (m *ChatStreamMessage) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_ChatStreamMessage.Marshal(b, m, deterministic)
+}
+func (m *ChatStreamMessage) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ChatStreamMessage.Merge(m, src)
+}
+func (m *ChatStreamMessage) XXX_Size() int {
+	return xxx_messageInfo_ChatStreamMessage.Size(m)
+}
+func (m *ChatStreamMessage) XXX_DiscardUnknown() {
+	xxx_messageInfo_ChatStreamMessage.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ChatStreamMessage proto.InternalMessageInfo
+
+func (m *ChatStreamMessage) GetName() string {
+	if m != nil {
+		return m.Name
+	}
+	return ""
+}
+
+func (m *ChatStreamMessage) GetMessage() string {
+	if m != nil {
+		return m.Message
+	}
+	return ""
+}
+
+type ChatStreamResponse struct {
+	Message              string   `protobuf:"bytes,1,opt,name=message,proto3" json:"message,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *ChatStreamResponse) Reset()         { *m = ChatStreamResponse{} }
+func (m *ChatStreamResponse) String() string { return proto.CompactTextString(m) }
+func (*ChatStreamResponse) ProtoMessage()    {}
+func (*ChatStreamResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_34fd3b300f068c9c, []int{3}
+}
+
+func (m *ChatStreamResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_ChatStreamResponse.Unmarshal(m, b)
+}
+func (m *ChatStreamResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_ChatStreamResponse.Marshal(b, m, deterministic)
+}
+func (m *ChatStreamResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ChatStreamResponse.Merge(m, src)
+}
+func (m *ChatStreamResponse) XXX_Size() int {
+	return xxx_messageInfo_ChatStreamResponse.Size(m)
+}
+func (m *ChatStreamResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_ChatStreamResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ChatStreamResponse proto.InternalMessageInfo
+
+func (m *ChatStreamResponse) GetMessage() string {
+	if m != nil {
+		return m.Message
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*FilmEmptyRequest)(nil), "v1.FilmEmptyRequest")
 	proto.RegisterType((*FilmCategory)(nil), "v1.FilmCategory")
+	proto.RegisterType((*ChatStreamMessage)(nil), "v1.ChatStreamMessage")
+	proto.RegisterType((*ChatStreamResponse)(nil), "v1.ChatStreamResponse")
 }
 
 func init() { proto.RegisterFile("film-service.proto", fileDescriptor_34fd3b300f068c9c) }
 
 var fileDescriptor_34fd3b300f068c9c = []byte{
-	// 207 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x5c, 0x8f, 0x41, 0x4b, 0xc4, 0x30,
-	0x10, 0x46, 0x4d, 0x5c, 0xc5, 0x1d, 0x45, 0x96, 0x41, 0x21, 0xe8, 0xa5, 0xf4, 0xb4, 0x17, 0x8b,
-	0xab, 0x3f, 0x61, 0x71, 0x4f, 0x9e, 0xba, 0x78, 0x96, 0xd8, 0x8e, 0x75, 0x20, 0x69, 0xda, 0x24,
-	0x2d, 0xf4, 0xdf, 0x8b, 0xa9, 0x05, 0xf1, 0x36, 0xbc, 0x77, 0x98, 0xf7, 0x01, 0x7e, 0xb2, 0xb1,
-	0x0f, 0x81, 0xfc, 0xc8, 0x15, 0x15, 0x9d, 0x77, 0xd1, 0xa1, 0x1c, 0x77, 0x39, 0xc2, 0xe6, 0xc0,
-	0xc6, 0xbe, 0xd8, 0x2e, 0x4e, 0x25, 0xf5, 0x03, 0x85, 0x98, 0xf7, 0x70, 0xf5, 0xc3, 0xf6, 0x3a,
-	0x52, 0xe3, 0xfc, 0x84, 0xd7, 0x20, 0xb9, 0x56, 0x22, 0x13, 0xdb, 0xb3, 0x52, 0x72, 0x8d, 0x08,
-	0xab, 0x56, 0x5b, 0x52, 0x32, 0x13, 0xdb, 0x75, 0x99, 0x6e, 0xcc, 0xe0, 0xb2, 0xa6, 0x50, 0x79,
-	0xee, 0x22, 0xbb, 0x56, 0x9d, 0x26, 0xf5, 0x17, 0xe1, 0x3d, 0xac, 0xd9, 0xea, 0x86, 0xde, 0x07,
-	0x6f, 0xd4, 0x2a, 0xf9, 0x8b, 0x04, 0xde, 0xbc, 0x79, 0x3a, 0xce, 0x2f, 0xc3, 0x71, 0x0e, 0xc4,
-	0x3d, 0xdc, 0xbe, 0x72, 0x88, 0x07, 0x1d, 0xbe, 0xd8, 0xb5, 0xbf, 0x25, 0x4c, 0x01, 0x6f, 0x8a,
-	0x71, 0x57, 0xfc, 0x2f, 0xbe, 0xdb, 0x2c, 0x74, 0x69, 0xce, 0x4f, 0x1e, 0xc5, 0xc7, 0x79, 0x9a,
-	0xf9, 0xfc, 0x1d, 0x00, 0x00, 0xff, 0xff, 0xcd, 0x05, 0x5a, 0xfc, 0xfc, 0x00, 0x00, 0x00,
+	// 281 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x64, 0x91, 0xc1, 0x4e, 0xb3, 0x40,
+	0x14, 0x85, 0x3b, 0xfc, 0xfd, 0xd5, 0xde, 0x1a, 0x53, 0x6f, 0xac, 0x21, 0x75, 0x43, 0x66, 0xc5,
+	0x46, 0xd2, 0xea, 0x13, 0x28, 0xb1, 0x2b, 0xdd, 0x40, 0x5c, 0x9b, 0xb1, 0x5c, 0xe9, 0x24, 0x0c,
+	0x43, 0x67, 0xa6, 0x24, 0x7d, 0x12, 0x5f, 0xd7, 0x14, 0x8a, 0x16, 0xdd, 0xc1, 0x77, 0x4e, 0xe6,
+	0xcc, 0x39, 0x03, 0xf8, 0x21, 0x0b, 0x75, 0x6b, 0xc9, 0xd4, 0x72, 0x45, 0x51, 0x65, 0xb4, 0xd3,
+	0xe8, 0xd5, 0x0b, 0x8e, 0x30, 0x59, 0xca, 0x42, 0x3d, 0xa9, 0xca, 0xed, 0x12, 0xda, 0x6c, 0xc9,
+	0x3a, 0xbe, 0x81, 0xf3, 0x3d, 0x8b, 0x85, 0xa3, 0x5c, 0x9b, 0x1d, 0x5e, 0x80, 0x27, 0x33, 0x9f,
+	0x05, 0x2c, 0xfc, 0x9f, 0x78, 0x32, 0x43, 0x84, 0x61, 0x29, 0x14, 0xf9, 0x5e, 0xc0, 0xc2, 0x51,
+	0xd2, 0x7c, 0x63, 0x00, 0xe3, 0x8c, 0xec, 0xca, 0xc8, 0xca, 0x49, 0x5d, 0xfa, 0xff, 0x1a, 0xe9,
+	0x18, 0xe1, 0x0d, 0x8c, 0xa4, 0x12, 0x39, 0xbd, 0x6d, 0x4d, 0xe1, 0x0f, 0x1b, 0xfd, 0xac, 0x01,
+	0xaf, 0xa6, 0xe0, 0x0f, 0x70, 0x19, 0xaf, 0x85, 0x4b, 0x9d, 0x21, 0xa1, 0x5e, 0xc8, 0x5a, 0x91,
+	0xd3, 0x77, 0x0e, 0x3b, 0xca, 0xf1, 0xe1, 0x54, 0xb5, 0xf2, 0x21, 0xbe, 0xfb, 0xe5, 0x11, 0xe0,
+	0xcf, 0x11, 0x09, 0xd9, 0x4a, 0x97, 0xb6, 0xe7, 0x67, 0x3d, 0xff, 0xdd, 0x27, 0x6b, 0x6b, 0xda,
+	0xb4, 0x1d, 0x05, 0x63, 0x98, 0x3e, 0x4b, 0xeb, 0x96, 0xc2, 0xae, 0xa5, 0x2e, 0x0f, 0xed, 0x25,
+	0x59, 0xbc, 0x8a, 0xea, 0x45, 0xf4, 0x7b, 0xa5, 0xd9, 0xa4, 0xa3, 0xdd, 0x4e, 0x7c, 0x30, 0x67,
+	0xf8, 0x08, 0xe3, 0x94, 0xca, 0xac, 0xab, 0x30, 0xdd, 0x9b, 0xfe, 0x34, 0x9b, 0x5d, 0xf7, 0x71,
+	0x77, 0x5b, 0x3e, 0x08, 0xd9, 0x9c, 0xbd, 0x9f, 0x34, 0xcf, 0x73, 0xff, 0x15, 0x00, 0x00, 0xff,
+	0xff, 0xf2, 0x2b, 0xa2, 0xa1, 0xb4, 0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -155,6 +248,7 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type FilmsServiceClient interface {
 	ListFashionCategories(ctx context.Context, in *FilmEmptyRequest, opts ...grpc.CallOption) (FilmsService_ListFashionCategoriesClient, error)
+	SendMessage(ctx context.Context, opts ...grpc.CallOption) (FilmsService_SendMessageClient, error)
 }
 
 type filmsServiceClient struct {
@@ -197,9 +291,41 @@ func (x *filmsServiceListFashionCategoriesClient) Recv() (*FilmCategory, error) 
 	return m, nil
 }
 
+func (c *filmsServiceClient) SendMessage(ctx context.Context, opts ...grpc.CallOption) (FilmsService_SendMessageClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_FilmsService_serviceDesc.Streams[1], "/v1.FilmsService/SendMessage", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &filmsServiceSendMessageClient{stream}
+	return x, nil
+}
+
+type FilmsService_SendMessageClient interface {
+	Send(*ChatStreamMessage) error
+	Recv() (*ChatStreamResponse, error)
+	grpc.ClientStream
+}
+
+type filmsServiceSendMessageClient struct {
+	grpc.ClientStream
+}
+
+func (x *filmsServiceSendMessageClient) Send(m *ChatStreamMessage) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *filmsServiceSendMessageClient) Recv() (*ChatStreamResponse, error) {
+	m := new(ChatStreamResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // FilmsServiceServer is the server API for FilmsService service.
 type FilmsServiceServer interface {
 	ListFashionCategories(*FilmEmptyRequest, FilmsService_ListFashionCategoriesServer) error
+	SendMessage(FilmsService_SendMessageServer) error
 }
 
 // UnimplementedFilmsServiceServer can be embedded to have forward compatible implementations.
@@ -208,6 +334,9 @@ type UnimplementedFilmsServiceServer struct {
 
 func (*UnimplementedFilmsServiceServer) ListFashionCategories(req *FilmEmptyRequest, srv FilmsService_ListFashionCategoriesServer) error {
 	return status.Errorf(codes.Unimplemented, "method ListFashionCategories not implemented")
+}
+func (*UnimplementedFilmsServiceServer) SendMessage(srv FilmsService_SendMessageServer) error {
+	return status.Errorf(codes.Unimplemented, "method SendMessage not implemented")
 }
 
 func RegisterFilmsServiceServer(s *grpc.Server, srv FilmsServiceServer) {
@@ -235,6 +364,32 @@ func (x *filmsServiceListFashionCategoriesServer) Send(m *FilmCategory) error {
 	return x.ServerStream.SendMsg(m)
 }
 
+func _FilmsService_SendMessage_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(FilmsServiceServer).SendMessage(&filmsServiceSendMessageServer{stream})
+}
+
+type FilmsService_SendMessageServer interface {
+	Send(*ChatStreamResponse) error
+	Recv() (*ChatStreamMessage, error)
+	grpc.ServerStream
+}
+
+type filmsServiceSendMessageServer struct {
+	grpc.ServerStream
+}
+
+func (x *filmsServiceSendMessageServer) Send(m *ChatStreamResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *filmsServiceSendMessageServer) Recv() (*ChatStreamMessage, error) {
+	m := new(ChatStreamMessage)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 var _FilmsService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "v1.FilmsService",
 	HandlerType: (*FilmsServiceServer)(nil),
@@ -244,6 +399,12 @@ var _FilmsService_serviceDesc = grpc.ServiceDesc{
 			StreamName:    "ListFashionCategories",
 			Handler:       _FilmsService_ListFashionCategories_Handler,
 			ServerStreams: true,
+		},
+		{
+			StreamName:    "SendMessage",
+			Handler:       _FilmsService_SendMessage_Handler,
+			ServerStreams: true,
+			ClientStreams: true,
 		},
 	},
 	Metadata: "film-service.proto",
--- src/pkg/service/v1/film-service.go
+++ src/pkg/service/v1/film-service.go
@@ -2,27 +2,28 @@ package v1
 
 import (
 	"context"
-	"crypto/rsa"
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/tuanpa28/film-service/src/pkg/api/v1"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/status"
+	"io"
 	"io/ioutil"
 	"os"
 	"sync"
+	"time"
 )
 
 
 type FilmServiceServer struct {
 	database *sql.DB
-	jwtPrivatekey *rsa.PrivateKey
 	savedCategories [] *v1.FilmCategory
 
 	mu sync.Mutex
 }
+
 
 func (s *FilmServiceServer) ListFashionCategories(empty *v1.FilmEmptyRequest,stream v1.FilmsService_ListFashionCategoriesServer) error {
 	for _, category := range  s.savedCategories {
@@ -46,14 +47,32 @@ func (s * FilmServiceServer) connect(ctx context.Context) (*sql.Conn, error) {
 func NewFilmServiceServer(database *sql.DB) v1.FilmsServiceServer {
 	var service = FilmServiceServer{database: database}
 	service.loadLocalDataForSavedCategories()
-
-	key, _ := ioutil.ReadFile("./src/configs/jwt/private.pem")
-	parsedKey, err := jwt.ParseRSAPrivateKeyFromPEM(key)
-	if err != nil {
-		print(err)
-	}
-	service.jwtPrivatekey = parsedKey
 	return &service
+}
+
+func (s *FilmServiceServer) SendMessage(stream v1.FilmsService_SendMessageServer) error {
+	for {
+		in, err := stream.Recv()
+		// end of the streaming
+		if err == io.EOF {
+			grpclog.Println("server -- finished stream")
+			return nil
+		}
+		if err != nil {
+			grpclog.Printf("returned with error %v", err)
+			return err
+		}
+		message := in.Message
+		name := in.Name
+		if name == "" {
+			name = "Unknown"
+		}
+		grpclog.Printf("server -- received message:\n%v: %v", name, message)
+		revMsg := "received" + time.Now().Format("2006-01-02 15:04:05")
+		stream.Send(&v1.ChatStreamResponse{
+			Message: revMsg,
+		})
+	}
 }
 
 func (s * FilmServiceServer) loadLocalDataForSavedCategories() {
@@ -67,3 +86,5 @@ func (s * FilmServiceServer) loadLocalDataForSavedCategories() {
 	json.Unmarshal(byteValue, &s.savedCategories)
 	defer jsonFile.Close()
 }
+
+
